779f0ef405797ec4d4fbaa849df14e77ea821dcb
irc-chris <ishawniachristopher@gmail.com>
2026-03-02 11:24:56 -0600

    feat: writes header to counts

--- step5_counts.sh
+++ step5_counts.sh
@@ -33,12 +33,15 @@ HAP2_BAM="$outputdir/hap2.sorted.bam"
 # In haploid mode they come from splitting a single alignment via WhatsHap HP tags.
 # Either way, hap1 is counted against bedfile1 and hap2 against bedfile2.
 
+echo -e "chr\tstart\tend\tcount" > "$outputdir/hap1_counts.tsv"
+echo -e "chr\tstart\tend\tcount" > "$outputdir/hap2_counts.tsv"
+
 echo "$(date) Getting counts: hap1 vs bedfile1..."
 bedtools multicov -bams "$HAP1_BAM" -bed "$bedfile1" > "$outputdir/hap1_counts.tsv"
 echo "Counts written to $outputdir/hap1_counts.tsv"
 
 echo "$(date) Getting counts: hap2 vs bedfile2..."
-bedtools multicov -bams "$HAP2_BAM" -bed "$bedfile2" > "$outputdir/hap2_counts.tsv"
+bedtools multicov -bams "$HAP2_BAM" -bed "$bedfile2" >> "$outputdir/hap2_counts.tsv"
 echo "Counts written to $outputdir/hap2_counts.tsv"
 
 echo "$(date) Counts complete."
